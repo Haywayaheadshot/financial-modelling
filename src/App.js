e15bdb6be58ccbec9f75f7097ca84b90f1121175
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-28 14:30:45 -0600

    Add Browser react to handle page naviagation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './styles/App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Stock Data</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/details" element={<Details />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
