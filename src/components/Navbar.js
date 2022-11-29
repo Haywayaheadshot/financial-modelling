5617fd8847eb4082d2c2e3a79e019f06acc322e9
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-29 09:29:01 -0600

    Add navlink to logo name and image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,12 @@ export default function Navbar() {
   return (
     <div className="nav-container">
       <section className="logo-container">
-        <img className="logo" src={logo} alt="Webapage Logo" />
-        <h1>Stocks Info</h1>
+        <NavLink className="desktop-ul-li" to="/">
+          <img className="logo" src={logo} alt="Webapage Logo" />
+        </NavLink>
+        <NavLink className="desktop-ul-li" to="/details">
+          <h1>Stocks Info</h1>
+        </NavLink>
       </section>
       <section className="for-phone">
         <Hamburger
@@ -47,17 +51,17 @@ export default function Navbar() {
       <section className="for-desktop">
         <ul className="desktop-ul">
           <li>
-            <NavLink className="desktop-ul-li" onClick={() => isClosed(false)} to="/" activeclassname="active-link">
+            <NavLink className="desktop-ul-li" to="/" activeclassname="active-link">
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink className="desktop-ul-li" onClick={() => isClosed(false)} to="/details" activeclassname="active-link">
+            <NavLink className="desktop-ul-li" to="/details" activeclassname="active-link">
               Details
             </NavLink>
           </li>
           <li>
-            <NavLink className="desktop-ul-li" onClick={() => isClosed(false)} to="/contact" activeclassname="active-link">
+            <NavLink className="desktop-ul-li" to="/contact" activeclassname="active-link">
               Contact
             </NavLink>
           </li>
