ab251334ffbc868c010578a5063310790a63da04
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-30 15:14:21 -0600

    Add className to tbody td

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCoins } from '../redux/home/Home';
+import '../styles/details.css';
 
 export default function Details() {
   const params = useParams();
@@ -12,56 +13,60 @@ export default function Details() {
   }, []);
   const filteredCoin = coins.filter((coin) => (coin.id === params.id));
   return (
-    <div>
+    <div className="details-container">
       <NavLink to="../">
-        <button type="button">Back</button>
+        <button className="back-button" type="button">Back</button>
       </NavLink>
       { filteredCoin.map((coin) => (
-        <section key={coin.id} id={coin.id}>
+        <section className="detail-mapped-container" key={coin.id} id={coin.id}>
           <div>
-            <h1>
+            <h1 className="header-details-key">
               {coin.name}
-              /BTC
+              /
+              {coin.symbol}
             </h1>
-            <h2>Rank</h2>
+            <section className="coin-rank-container">
+              <h2 className="header-details-key">Coin Rank:</h2>
+              <h1 className="header-details-value">{coin.rank}</h1>
+            </section>
           </div>
           <table>
-            <tbody>
+            <tbody className="details-column-data-body">
               <tr>
-                <td className="column-data">{`Total Supply: ${coin.totalSupply}`}</td>
-                <td className="column-data">102220</td>
+                <td className="details-column-data">Price Against USD</td>
+                <td className="details-column-data">{coin.price}</td>
               </tr>
               <tr>
-                <td className="column-data">Price Against BTC</td>
-                <td className="column-data">102220</td>
+                <td className="details-column-data">Price Against BTC</td>
+                <td className="details-column-data">{coin.priceBtc}</td>
               </tr>
               <tr>
-                <td className="column-data">Volume</td>
-                <td className="column-data">102220</td>
+                <td className="details-column-data">Volume</td>
+                <td className="details-column-data">{coin.volume}</td>
               </tr>
               <tr>
-                <td className="column-data">Market Cap</td>
-                <td className="column-data">102220</td>
+                <td className="details-column-data">Market Cap</td>
+                <td className="details-column-data">{coin.marketCap}</td>
               </tr>
               <tr>
-                <td className="column-data">Available Supply</td>
-                <td className="column-data">102220</td>
+                <td className="details-column-data">Available Supply</td>
+                <td className="details-column-data">{coin.availableSupply}</td>
               </tr>
               <tr>
-                <td className="column-data">Total Supply</td>
-                <td className="column-data">102220</td>
+                <td className="details-column-data">Total Supply</td>
+                <td className="details-column-data">{coin.totalSupply}</td>
               </tr>
               <tr>
-                <td className="column-data">Hourly Price Change</td>
-                <td className="column-data">102220</td>
+                <td className="details-column-data">Price Change(1h)</td>
+                <td className="details-column-data">{coin.priceChange1h}</td>
               </tr>
               <tr>
-                <td className="column-data">Weekly Price Change</td>
-                <td className="column-data">102220</td>
+                <td className="details-column-data">Price Change(1D)</td>
+                <td className="details-column-data">{coin.priceChange1d}</td>
               </tr>
               <tr>
-                <td className="column-data">Daily Price Change</td>
-                <td className="column-data">102220</td>
+                <td className="details-column-data">Price Change(1W)</td>
+                <td className="details-column-data">{coin.priceChange1w}</td>
               </tr>
             </tbody>
           </table>
