3446ca285991c9cbecbb7f5a3444aa9bbfaf8583
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-30 17:43:14 -0600

    Add correct path for details

--- src/__test__/Details.test.js
+++ src/__test__/Details.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { Provider } from 'react-redux';
-import Details from '../components/Details';
+import Details from '../pages/Details';
 import store from '../redux/configureStore';
 
 describe('Testing Details component', () => {
