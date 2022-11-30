b6587abfcc52d31c9c2c442d5ba835216d889072
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-30 10:09:33 -0600

    Fix eslint error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,9 @@ import React from 'react';
 import HomeThumbnail from '../components/HomeThumbnail';
 
 export default function Home() {
-  const openMoreHandler = () => {
-    console.log('navigate to details with this id');
-  };
   return (
     <div>
-      {/* { coins.map((coin) => (
-        ))} */}
-      <HomeThumbnail
-        onClick={openMoreHandler}
-      />
+      <HomeThumbnail />
     </div>
   );
 }
