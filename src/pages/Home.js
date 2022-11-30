ed853c0569b922245c762b72516df203b7c000fd
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-30 06:09:57 -0600

    Delete map

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,17 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import HomeThumbnail from '../components/HomeThumbnail';
-import { getCoins } from '../redux/home/Home';
 
 export default function Home() {
-  const coins = useSelector((state) => state.coins);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getCoins());
-  }, [dispatch]);
-
   const openMoreHandler = () => {
     console.log('navigate to details with this id');
   };
   return (
     <div>
-      { coins.map((coin) => (
-        <HomeThumbnail
-          onClick={openMoreHandler}
-          key={coin.id}
-          id={coin.id}
-          name={coin.name}
-          symbol={coin.symbol}
-          price={coin.price}
-          priceBtc={coin.priceBtc}
-          marketCap={coin.marketCap}
-          availableSupply={coin.availableSupply}
-          icon={coin.icon}
-        />
-      ))}
+      {/* { coins.map((coin) => (
+        ))} */}
+      <HomeThumbnail
+        onClick={openMoreHandler}
+      />
     </div>
   );
 }
