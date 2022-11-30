266c68f4dc392ec1ce023affce26f9551a213f34
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-30 16:58:02 -0600

    Add details test

---
+++ src/__test__/Details.test.js
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { Provider } from 'react-redux';
+import Details from '../components/Details';
+import store from '../redux/configureStore';
+
+describe('Testing Details component', () => {
+  it('Should match the snapshot Detail', () => {
+    const detail = render(
+      <StrictMode>
+        <Router>
+          <Provider store={store}>
+            <Details />
+          </Provider>
+        </Router>
+      </StrictMode>,
+    );
+    expect(detail).toMatchSnapshot();
+  });
+});
