eaa9cb259011daba84fde84d438fd0822d808b7e
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-30 06:10:21 -0600

    Add map

--- src/components/HomeThumbnail.js
+++ src/components/HomeThumbnail.js
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import HomeSearch from './dep/HomeSearch';
 import '../styles/home-thumbnail.css';
+import { getCoins } from '../redux/home/Home';
 
 export default function HomeThumbnail({
-  id, name, symbol, icon, price, priceBtc,
+  icon,
 }) {
+  const coins = useSelector((state) => state.coins);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCoins());
+  }, [dispatch]);
+
   const background = {
     background: `rgb(93, 141, 180) url(${icon}) no-repeat top center`,
   };
   return (
     <div>
       <div className="home-coin-container">
-        <button id={id} type="button" style={background} className="coin-button">
-          <section className="coin-button-header">
-            <h3 className="header-price">{name}</h3>
-            <h3>{symbol}</h3>
-          </section>
-          <section>
-            <h3 className="price-tag">
-              Price Against USD:
-              <br />
-              <b className="actual-price">{price}</b>
-            </h3>
-            <h3 className="price-tag">
-              Price Against BTC:
-              <br />
-              <b className="actual-price">{priceBtc}</b>
-            </h3>
-            {/* <h3 className="price-tag">
+        {coins.map((coin) => (
+          <button key={coin.id} id={coin.id} type="button" style={background} className="coin-button">
+            <section className="coin-button-header">
+              <h3 className="header-price">{coin.name}</h3>
+              <h3>{coin.symbol}</h3>
+            </section>
+            <section>
+              <h3 className="price-tag">
+                Price Against USD:
+                <br />
+                <b className="actual-price">{coin.price}</b>
+              </h3>
+              <h3 className="price-tag">
+                Price Against BTC:
+                <br />
+                <b className="actual-price">{coin.priceBtc}</b>
+              </h3>
+              {/* <h3 className="price-tag">
               Market Cap:
               <br />
               <b className="actual-price">{marketCap}</b>
@@ -38,9 +47,10 @@ export default function HomeThumbnail({
               <br />
               <b className="actual-price">{availableSupply}</b>
             </h3> */}
-          </section>
-          <p className="click-for-more">Click For More Info</p>
-        </button>
+            </section>
+            <p className="click-for-more">Click For More Info</p>
+          </button>
+        ))}
       </div>
       <section className="home-search-section">
         <HomeSearch />
@@ -50,10 +60,10 @@ export default function HomeThumbnail({
 }
 
 HomeThumbnail.propTypes = {
-  id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  symbol: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  priceBtc: PropTypes.number.isRequired,
+  // id: PropTypes.string.isRequired,
+  // name: PropTypes.string.isRequired,
+  // symbol: PropTypes.string.isRequired,
+  // price: PropTypes.number.isRequired,
+  // priceBtc: PropTypes.number.isRequired,
   icon: PropTypes.string.isRequired,
 };
