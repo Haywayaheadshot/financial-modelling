77b1a50a9bbc8eeb52be0b7ca5120da2e56ce5fd
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-30 05:39:32 -0600

    Use and import useEffect, useDispatch and  map object from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import HomeThumbnail from '../components/HomeThumbnail';
+import { getCoins } from '../redux/home/Home';
 
 export default function Home() {
+  const coins = useSelector((state) => state.coins);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCoins());
+  }, [dispatch]);
+  console.log(coins);
+
+  const openMoreHandler = () => {
+    console.log('navigate to details with this id');
+  };
   return (
     <div>
-      <h1>Home</h1>
+      { coins.map((coin) => (
+        <HomeThumbnail
+          onClick={openMoreHandler}
+          key={coin.id}
+          id={coin.id}
+          name={coin.name}
+          symbol={coin.symbol}
+          price={coin.price}
+          priceBtc={coin.priceBtc}
+          marketCap={coin.marketCap}
+          availableSupply={coin.availableSupply}
+          icon={coin.icon}
+        />
+      ))}
     </div>
   );
 }
